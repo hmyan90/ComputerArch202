85dac167f3e61ed94e5c197725f61e78d1c91d7d
hmyan90 <34290458+hmyan90@users.noreply.github.com>
2018-03-09 01:45:36 -0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#docker image pull hmyan19901001/cython
+
+# kernel version
+kernel_version=`uname -r`
+echo "The kernel version of this computer is: ${kernel_version}"
+
+rm hyan90_proj -fr
+mkdir hyan90_proj
+cd hyan90_proj
+git clone https://github.com/hmyan90/ComputerArch202.git
+cd ComputerArch202
+sed -i "s/KERNEL_VERSION/${kernel_version}/g" Dockerfile
+cat Dockerfile
+docker build -t hyan90 .
+
